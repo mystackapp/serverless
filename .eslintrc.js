02cb8c141381ad57c1451777083e00bd4b2470ca
Nick Yanush <m.yanush@apiko.com>
2022-11-10 22:12:15 +0200

    Add registration, eslint, prettier

---
+++ .eslintrc.js
@@ -0,0 +1,58 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/typescript',
+    'standard-with-typescript',
+    'eslint:recommended',
+    'plugin:import/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
+  },
+  plugins: ['react', 'import', 'prettier'],
+  rules: {
+    'dot-notation': ['error', { allowPattern: '^[a-z]+(_[a-z]+)+$' }],
+    '@typescript-eslint/explicit-function-return-type': 'warn',
+    '@typescript-eslint/consistent-type-assertions': 'warn',
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'no-multiple-empty-lines': 'error',
+    'eol-last': ['error', 'always'],
+    'object-curly-spacing': ['error', 'always'],
+    'import/no-unresolved': 'error',
+    'prettier/prettier': ['error', { singleQuote: true }],
+    'max-len': [
+      'error',
+      {
+        code: 80,
+        tabWidth: 2,
+        ignoreComments: true, // "comments": 80
+        ignoreUrls: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+      },
+    },
+  },
+};
